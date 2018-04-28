9e68782e6e8c47129786039733bbee909e00ed25
Mike <mwolf84@gmail.com>
2018-04-28 15:31:50 -0700

    Continued to clean up discover and search views

--- application/views/discover.php
+++ application/views/discover.php
@@ -107,15 +107,15 @@
 					</div>
 					</a>
 					
-					<div style="display: flex; margin-top: -10px;">						
+					<div style="display: flex; margin-top: 10px;">						
 						<div class="reportBtnShell" data-peerID="<?=$listing->relationships->vendor->data->peerID?>" data-slug="<?=$listing->data->slug?>" data-tip="Report this listing" style="margin-top:-25px;margin-left:5px;flex:1;display:none;">
-						  <button class="iconBtnTn clrP clrBr tx2 " style="width: 30px;padding:0;height: 30px;cursor:pointer;background-color:white;">
+						  <button class="iconBtnTn button clrP clrBr tx2 " style="width: 30px;padding:0;height: 30px;cursor:pointer;background-color:white;">
 						    <img src="<?=asset_url()?>img/ios7-flag.png" width=24 />
 						  </button>
 						</div>
 							
 							<div style="flex:1">
-						<div class="Search-Avatar-Circle" style="z-index:1000;float:right;margin-top:-21px;background-image: url('<?php echo (($listing->relationships->vendor->data->avatarHashes->small!="")) ? "https://gateway.ob1.io/ob/images/".$listing->relationships->vendor->data->avatarHashes->small : asset_url()."img/defaultAvatar.png"?>');" title="<?=$listing->relationships->vendor->data->name?>" onclick="location.href='/store/<?=$listing->relationships->vendor->data->peerID?>'"></div>
+						<div class="Search-Avatar-Circle" style="z-index:10;background-image: url('<?php echo (($listing->relationships->vendor->data->avatarHashes->small!="")) ? "https://gateway.ob1.io/ob/images/".$listing->relationships->vendor->data->avatarHashes->small : asset_url()."img/defaultAvatar.png"?>');" title="<?=$listing->relationships->vendor->data->name?>" onclick="location.href='/store/<?=$listing->relationships->vendor->data->peerID?>'"></div>
 							</div>
 				
 					</div>
