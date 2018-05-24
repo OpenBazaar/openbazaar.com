82d1883943238c64562aea894c6976036f2574d2
Brian Hoffman <brianchoffman@gmail.com>
2018-05-24 14:47:34 -0400

    Clean up unknown text

--- application/views/discover.php
+++ application/views/discover.php
@@ -234,7 +234,7 @@
 											$inventory = $crypto_listing->data->totalInventoryQuantity / $crypto_listing->data->coinDivisibility;
 											echo number_format($inventory). " " . $crypto_listing->data->coinType;
 										} else { 
-											echo '?';
+											echo '<span class="unknown">Unknown</span>';
 										}																		
 									?> &nbsp; <img src="<?=asset_url()?>img/coins/64x64/<?=coin_to_icon($crypto_listing->data->coinType)?>.png" width=18 height=18/>
 								</div>
