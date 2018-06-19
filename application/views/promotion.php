4423f7555542c140a95e132cfa683aa57fa6702e
Mike <mwolf84@gmail.com>
2018-06-19 13:11:39 -0700

    Made links to .org open in new tab

--- application/views/promotion.php
+++ application/views/promotion.php
@@ -3,11 +3,11 @@
     <div class="Listing-Box">
       <h1 style="text-align: center;">Receive $10 in Bitcoin</h1>
       <div style="text-align: center; margin-bottom: 10px">Install OpenBazaar Desktop and follow the simple steps below</div>
-      <a href="https://openbazaar.org/download" title="Download OpenBazaar"><img style="width: 100%" src="https://www.openbazaar.org/wp-content/uploads/2018/04/openbazaar-2.0-teaser.jpg" /></a>
+      <a href="https://openbazaar.org/download" title="Download OpenBazaar" target="_blank"><img style="width: 100%" src="https://www.openbazaar.org/wp-content/uploads/2018/04/openbazaar-2.0-teaser.jpg" /></a>
       <div>
         <h2>Steps</h2>
         <ol>
-          <li style="margin-bottom: 10px;">Visit <a href="https://openbazaar.org/download" title="Download OpenBazaar">OpenBazaar.org/download</a> and install OpenBazaar for Windows, Mac or Linux.</li>
+          <li style="margin-bottom: 10px;">Visit <a href="https://openbazaar.org/download" title="Download OpenBazaar" target="_blank">OpenBazaar.org/download</a> and install OpenBazaar for Windows, Mac or Linux.</li>
           <li style="margin-bottom: 10px;">During setup choose <b>Bitcoin</b> as your coin type.</li>
           <li style="margin-bottom: 10px;">Vist the <a href="ob://QmSkkZpNFzWubxE4p9Ejjo2CLqq61jxuRF26ZzCmCKaHcJ/store">official OpenBazaar store</a>, click message, say hi and send code: <b><?php echo $code ?></b>.</li>
           <li style="margin-bottom: 10px;">Hang tight. We'll respond as soon as possible with <b>$10 in Bitcoin</b>.</li>
