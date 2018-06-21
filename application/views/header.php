d1b6eeab7d7afd450fff341e8d0a118eca403c77
Mike <mwolf84@gmail.com>
2018-06-21 12:53:49 -0700

    added styling to hide promotion bar

--- application/views/header.php
+++ application/views/header.php
@@ -125,7 +125,7 @@ setlocale(LC_ALL, $locale);
 			<span>Get $10 towards your first purchase on OpenBazaar!</span>
 			<img src="<?=asset_url()?>img/btcIcon128.png" style="position: absolute; width: 72px; top: -12px; opacity: .12; left: 48%" />
 			<a href="/bitcoin-promotion" style="text-decoration: underline; font-weight: 400; color: #fff; font-size: 13px; margin-left: 2px">Get started</a>
-			<img src="<?=asset_url()?>img/ios7-close-empty-white.png" style="position: absolute; right: 24px; top: 18px; width: 12px; cursor: pointer;" onclick="dismissBanner();" />
+			<img src="<?=asset_url()?>img/ios7-close-empty-white.png" style="position: absolute; right: 30px; top: 18px; width: 12px; cursor: pointer;" onclick="dismissBanner();" />
 		</div>
 
 		<div class="Rectangle-3">						
