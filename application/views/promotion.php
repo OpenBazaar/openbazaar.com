3bbd16132ce3de17ccb0a888e36be36c88b36399
Mike <mwolf84@gmail.com>
2018-06-20 17:25:09 -0700

    copy changes + added new share section

--- application/views/promotion.php
+++ application/views/promotion.php
@@ -1,16 +1,16 @@
 <div class="Rectangle-10 clearfix">
-  <div class="Page-Sub-Content" style="background: url(http://localhost:8080/assets/img/openbazaar-background.png)">
+  <div class="Page-Sub-Content">
     <div class="Listing-Box" style="padding-bottom: 0; text-align: center;">
       <img src="<?=asset_url()?>img/btcIcon128.png" style="position: absolute; width: 40px; top: 6px; left: calc(50% - 20px); z-index: 50" />
       <div style="height: 4px; width: 100%; position: absolute; top: 23px; left: 0; border-top-left-radius: 2px; border-top-right-radius: 2px; background: #f7921a"></div>
       <h1 style="text-align: center; margin: 10px 0 0 0; font-size: 30px;">Get $10 in Bitcoin</h1>
-      <div style="text-align: center; margin-bottom: 5px; color: #777">When you try OpenBazaar</div>
+      <div style="text-align: center; margin-bottom: 5px; color: #777">Help build the community — Get $10 towards your first purchase on OpenBazaar.</div>
 
       <!-- social sharing stuff -->
       <div style="margin-bottom: 5px">
-        <a href="https://twitter.com/intent/tweet?text=🤑 Get $10 in Bitcoin when you try @OpenBazaar %23bitcoin %23btc" target="_blank" title="Share on Twitter"><img src="<?=asset_url()?>img/icon-twitter.png" height=12.5 style="margin-right: 5px" /></a>
+        <a href="https://twitter.com/intent/tweet?text=Help build the @OpenBazaar community — Get $10 towards your first purchase on %23OpenBazaar. 🤑 %23bitcoin %23btc" target="_blank" title="Share on Twitter"><img src="<?=asset_url()?>img/icon-twitter.png" height=12.5 style="margin-right: 5px" /></a>
         <a href="https://www.facebook.com/sharer/sharer.php?u=<?=base_url()?>/bitcoin-promotion" target="_blank"><img src="<?=asset_url()?>img/icon-facebook.png" height=12.5 style="margin-right: 5px" target="_blank" title="Share on Facebook"/></a>
-        <a href="http://pinterest.com/pin/create/button/?url=<?=base_url()?>/bitcoin-promotion&media=https://www.openbazaar.org/wp-content/uploads/2018/04/openbazaar-2.0-teaser.jpg&description=Get $10 in Bitcoin when you try OpenBazaar" target="_blank"  title="Share on Pinterest"><img src="<?=asset_url()?>img/icon-pinterest.png" height=12.5 target="_blank"/></a>
+        <a href="http://pinterest.com/pin/create/button/?url=<?=base_url()?>/bitcoin-promotion&media=https://www.openbazaar.org/wp-content/uploads/2018/04/openbazaar-2.0-teaser.jpg&description=Help build the OpenBazaar community — Get $10 towards your first purchase on OpenBazaar. 🤑" target="_blank"  title="Share on Pinterest"><img src="<?=asset_url()?>img/icon-pinterest.png" height=12.5 target="_blank"/></a>
       </div>
 
       <a href="https://openbazaar.org/download" title="Download OpenBazaar" target="_blank" class="Purchase-Button mobile-hidden" style="position: absolute; color: #fff; font-weight: 700; text-decoration: none; font-weight: 700; top: 288px; margin: auto; left: calc(50% - 110px); line-height: 45px; width: 215px; height: 46px; box-shadow: 2px 2px 14px #888888">DOWNLOAD</a>
@@ -29,9 +29,26 @@
     </div>
     <div class="Listing-Box" style="margin-top: 15px">
         <b>Want more?</b>
-        <div style="margin: 10px 0">Learn about all the latest OpenBazaar features & updates by joining our <a href="https://openbazaar.org/newsletter" title="OpenBazaar Newsletter" target="_blank">newsletter</a>.</div>
-        <div>Learn more about the project at <a href="https://openbazaar.org/" title="OpenBazaar" target="_blank">OpenBazaar.org</a></div>
+        <div style="margin: 10px 0">Learn about all the <b>latest OpenBazaar features & updates</b> by joining our <a href="https://openbazaar.org/newsletter" title="OpenBazaar Newsletter" target="_blank">newsletter</a>.</div>
+        <div><b>Learn more</b> about the project at <a href="https://openbazaar.org/" title="OpenBazaar" target="_blank">OpenBazaar.org</a></div>
       </div>
+
+    <div class="Listing-Box" style="margin-top: 15px">
+        <div style="margin-bottom: 8px"><b>Hook your friends up!</b></div>
+
+        <div style="margin-bottom: 5px">
+          <div>
+          <a href="https://twitter.com/intent/tweet?text=Help build the @OpenBazaar community — Get $10 towards your first purchase on %23OpenBazaar. 🤑 %23bitcoin %23btc" target="_blank" title="Share $10 on Twitter"><div class="Share-Button"><img src="<?=asset_url()?>img/icon-twitter.png" height=12.5 /> Share $10 on Twitter</div></a>
+          <a href="https://www.facebook.com/sharer/sharer.php?u=<?=base_url()?>/bitcoin-promotion" target="_blank"><div class="Share-Button"><img src="<?=asset_url()?>img/icon-facebook.png" height=12.5 style="margin-right: 5px" target="_blank" title="Share $10 on Facebook"/> Share $10 on Facebook</div></a>
+          <a href="http://pinterest.com/pin/create/button/?url=<?=base_url()?>/bitcoin-promotion&media=https://www.openbazaar.org/wp-content/uploads/2018/04/openbazaar-2.0-teaser.jpg&description=Help build the OpenBazaar community — Get $10 towards your first purchase on OpenBazaar. 🤑" target="_blank"  title="Share $10 on Pinterest"><div class="Share-Button"><img src="<?=asset_url()?>img/icon-pinterest.png" height=12.5 target="_blank"/> Share $10 on Pinterest</div></a>
+          </div>
+          <br />
+          <br />
+        </div>
+
+      </div>
+    </div>
+
     </div>
   </div>
 </div>
