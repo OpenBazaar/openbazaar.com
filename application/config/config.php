2f5018479ebffa9fd2a2a24f4dc12ccc1987f26f
Brian Hoffman <brianchoffman@gmail.com>
2018-07-23 12:12:34 -0400

    Update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://ob1.trade';
-$config['base_url'] = 'http://localhost:8080/';
+$config['base_url'] = 'http://openbazaar.com/';
 
 /*
 |--------------------------------------------------------------------------
