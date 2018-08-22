4c349ee236c9125cf38d459b57431515eedc1713
Brian Hoffman <brianchoffman@gmail.com>
2018-08-22 12:28:36 -0400

    Remove giveaway header

--- application/views/header.php
+++ application/views/header.php
@@ -120,13 +120,6 @@ setlocale(LC_ALL, $locale);
     //alert($(window).width());
 });
 </script>
-		<div class="Promotion-Bar mobile-hidden">
-			<img src="<?=asset_url()?>img/btcIcon128.png" style="position: relative; width: 18px; top: 5px; border-radius: 10px; border: solid 1px #fbfbfb;" />
-			<span>Get $10 towards your first purchase on OpenBazaar!</span>
-			<img src="<?=asset_url()?>img/btcIcon128.png" style="position: absolute; width: 72px; top: -12px; opacity: .12; left: 48%" />
-			<a href="/bitcoin-promotion" style="text-decoration: underline; font-weight: 400; color: #fff; font-size: 13px; margin-left: 2px">Get started</a>
-			<img src="<?=asset_url()?>img/ios7-close-empty-white.png" style="position: absolute; right: 30px; top: 18px; width: 12px; cursor: pointer;" onclick="dismissBanner();" />
-		</div>
 
 		<div class="Rectangle-3">						
 			
