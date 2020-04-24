4f0448676f47dc8493b003d8bec18250e91e0726
Brian Hoffman <brianchoffman@gmail.com>
2020-04-24 11:34:23 -0400

    Corona virus page

---
+++ application/views/coronavirus.php
@@ -0,0 +1,90 @@
+<div class="Rectangle-10 clearfix">
+  <div class="Page-Sub-Content">
+    
+    <div class="Listing-Box" style="margin-top: 60px">
+        <h2 style="margin-top: 0">Essential supplies needed to fight COVID-19</h2>
+        <p>People, states and hospitals all around the world are running very low on essential supplies to stay safe in these difficult times. If you, or anyone you know, can quickly produce, source or dropship <strong>face masks, N95 masks, surgical masks, hand sanitizer, hand soaps, ventilators, thermometers, wet wipes, toilet paper, etc.</strong> please get these items into circulation and into the right hands as soon as possible.  </p>
+        <p>Lives can be saved if we all do our part. The world needs your support to help get essential items into circulation. If you have access to a sewing machine, 3D printer, or even a distillery, please consider creating items and parts that can be donated and/or sold at a fair price. </p>
+        <p>Your essential items can be immediately distributed on OpenBazaar or Haven without fees. No account needed. No questions asked. Please do your part to help. </p>
+        
+        <div class="mobile-downloadbutton" style="display:flex;">
+        
+        <div style="flex-wrap: wrap;">
+        <a href="https://openbazaar.org/download"  style="text-decoration:none;">
+        <div class="downloadbox" style="margin-right:10px;display:flex;">
+	        
+	       
+	       	<div style="float:left;">
+		        <svg width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g filter="url(#filter0_d)">
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M2 4C2 2.89543 2.89543 2 4 2H36C37.1046 2 38 2.89543 38 4V36C38 37.1046 37.1046 38 36 38H26.4223H25.7375V35.48H15.248L14.1184 38H4C2.89543 38 2 37.1046 2 36V4Z" fill="white"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M8.30754 26.61L10.9225 37.9875H14.1632L19.9475 26.61L25.728 37.9875H28.9943L31.5874 26.61H8.30754Z" fill="#0474BE"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M14.1699 37.9466L10.8398 33.2083C10.7867 33.1328 10.8053 33.0286 10.8818 32.9752C10.8818 32.9752 13.9959 30.8028 15.5529 29.7166C15.753 29.5771 20.0062 26.61 20.0062 26.61L14.1699 37.9466Z" fill="white"/>
+				<g filter="url(#filter1_d)">
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M25.7079 37.9466L29.038 33.2083C29.0911 33.1328 29.0725 33.0286 28.996 32.9752L19.8716 26.61L25.7079 37.9466Z" fill="white"/>
+				</g>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M15.012 36.3361L19.949 26.61L24.8687 36.3361H15.012Z" fill="#17252D"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M32.9977 23.8676V23.8509C32.9139 23.6168 31.6409 20.0802 31.6325 20.0468C31.599 19.9465 29.3377 13.6508 29.2958 13.5505C28.8436 13.5756 28.2154 13.5756 27.4784 13.4836C26.7246 13.3916 25.2255 13.1994 23.6593 12.2797C21.9256 11.268 20.7866 9.87179 20.3511 9.09424C21.4901 11.5021 23.2657 14.5538 25.996 17.6473C28.5086 20.4983 30.9374 22.4797 32.9977 23.8676Z" fill="#287EE1"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M13.9859 17.455C16.7581 14.2863 18.5756 11.1845 19.7732 8.73477C19.7397 8.72641 18.9524 10.524 17.0596 11.7948C15.6275 12.7479 14.2204 12.9904 13.3327 13.1409C12.3612 13.3081 11.5236 13.3164 10.9374 13.2914C9.58895 16.7945 8.23217 20.2558 6.88376 23.7673L6.89213 23.8008H6.90051C8.99431 22.3209 11.4734 20.3394 13.9859 17.455Z" fill="#025A92"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M32.9977 23.8509C32.9977 23.8509 33.0061 23.8509 32.9977 23.8509C30.9374 22.4547 28.5086 20.4732 25.996 17.6306C23.2573 14.5371 21.4901 11.4771 20.3511 9.07755C20.259 8.87689 20.1669 8.67624 20.0747 8.48394C20.0161 8.46722 19.9658 8.44214 19.9156 8.42542C19.8737 8.51738 19.8235 8.61771 19.7732 8.70968C18.5839 11.1594 16.7581 14.2612 13.9859 17.4299C11.465 20.3144 8.99431 22.2708 6.89214 23.7423" fill="#1062A9"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M19.9156 26.6016L31.6409 26.61H32.4533L33.0144 23.8008C24.2372 23.8008 15.6526 23.7339 6.87538 23.7339L7.58727 26.6183H8.34942L19.9156 26.6016Z" fill="#025A92"/>
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M22.0931 8.83619C23.0814 8.80275 23.6007 8.47668 24.4885 8.26766C25.2674 8.07537 26.4231 7.94995 27.9558 8.30111C27.5957 7.88307 27.1015 7.37307 26.4315 6.88814C25.9206 6.52027 25.4349 6.25273 25.0161 6.05207C25.1836 5.73436 25.4265 5.35813 25.7615 4.99026C26.6828 3.98697 27.7967 3.59402 28.3829 3.43517C27.7632 3.26795 26.9424 3.13418 26.0128 3.23451C24.3963 3.40172 23.7431 4.12911 22.202 4.22107C21.5906 4.25452 20.8034 4.19599 19.8653 3.8532C19.8653 5.36649 19.8653 6.87142 19.8653 8.38471C19.9072 8.40143 19.9575 8.41816 19.9993 8.44324C20.0496 8.46832 20.1082 8.48504 20.1585 8.50176C20.6526 8.69406 21.3143 8.86127 22.0931 8.83619Z" fill="#DCDCDC"/>
+				</g>
+				<defs>
+				<filter id="filter0_d" x="0" y="0" width="40" height="40" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+				<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+				<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+				<feOffset/>
+				<feGaussianBlur stdDeviation="1"/>
+				<feColorMatrix type="matrix" values="0 0 0 0 0.439918 0 0 0 0 0.439918 0 0 0 0 0.439918 0 0 0 1 0"/>
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+				<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+				</filter>
+				<filter id="filter1_d" x="16.8716" y="23.61" width="15.1966" height="17.3366" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+				<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+				<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+				<feOffset/>
+				<feGaussianBlur stdDeviation="1.5"/>
+				<feColorMatrix type="matrix" values="0 0 0 0 0.488016 0 0 0 0 0.488016 0 0 0 0 0.488016 0 0 0 1 0"/>
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+				<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+				</filter>
+				</defs>
+				</svg>
+			</div>
+		
+
+			<div class="downloadboxText" style="font-weight:bold;float:left;margin-left:10px;line-height:40px;">Download OpenBazaar</div>
+
+		</div>
+        </a>
+        </div>
+        
+        <div style="flex-wrap: wrap;">
+	        <a href="https://gethaven.app" style="text-decoration:none;">	        
+	        <div class="downloadbox" style="align-items:center;display:flex;flex-wrap: wrap;">
+		        
+	
+		       	<div style="float:left;height:40px;align-items:center;display:flex;">
+			        <svg width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M15.0518 5.75315C15.2816 5.97876 15.3965 6.74778 15.3965 6.74778L16.0203 12.4301H16.2008L16.7228 6.74778C16.7228 6.74778 16.7495 6.06143 17.0051 5.75315C17.2606 5.44486 18.0733 5.11842 18.0733 5.11842L32 8.92151e-06C31.4743 2.40659 27.3857 4.18036 19.3662 7.44162L19.3092 8.0118L30.3286 3.82969C29.8608 6.22164 26.1345 7.95266 18.8256 11.1156L18.7811 11.6032L28.1472 7.71558C27.759 10.0683 18.6137 14.5521 18.6137 14.5521C18.6137 14.5521 17.1602 15.1825 16.0203 15.1825C14.8803 15.1825 13.4432 14.5521 13.4432 14.5521C13.4432 14.5521 4.24046 10.0677 3.85279 7.71558L13.2189 11.6032L13.1743 11.1162C5.86546 7.95267 2.13914 6.22164 1.67143 3.83028L12.6903 8.01179L12.6333 7.44161C4.61378 4.18034 0.525168 2.40658 0 0L13.7373 5.00499C13.7373 5.00499 14.7258 5.43311 15.0518 5.75315Z" fill="#00BF65"/>
+	</svg>
+	
+	
+				</div>
+	
+	
+				<div class="downloadboxText" style="font-weight:bold;float:left;margin-left:10px;line-height:40px;">Download Haven</div>
+	
+			</div></a>
+        </div>
+		        		
+        </div>
+
+		
+		
+        <p>Stay safe,<BR/>OpenBazaar</p>
+        
+    </div>
+      </div>
+</div>
