83a50dcaa6a6b677375f80a14b2337d883768ee5
Brian Hoffman <brianchoffman@gmail.com>
2020-06-08 11:55:20 -0400

    Remove autloading database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'database');
+// $autoload['libraries'] = array('session', 'database');
+$autoload['libraries'] = array('session');
 
 /*
 | -------------------------------------------------------------------
