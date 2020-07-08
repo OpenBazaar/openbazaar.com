53775c2791c7c4484a7e2fd41230403baeb5d7df
Brian Hoffman <brianchoffman@gmail.com>
2020-07-08 15:10:22 -0400

    Update script refresh

--- application/views/header.php
+++ application/views/header.php
@@ -106,7 +106,7 @@ if(!isset($tab)) {
 
 		<script src="//twemoji.maxcdn.com/2/twemoji.min.js?2.5"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="<?=asset_url()?>js/script.js?7"></script>
+		<script src="<?=asset_url()?>js/script.js?8"></script>
 		<script src="<?=asset_url()?>js/flickity.pkgd.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.7/jquery.lazy.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.7/jquery.lazy.plugins.min.js"></script>
