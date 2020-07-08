b8657d3052ba39191e08fffa74180757085c5764
Brian Hoffman <brianchoffman@gmail.com>
2020-07-08 11:32:04 -0400

    Mobile Footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -64,37 +64,13 @@
 </div>
 <?php } ?>
 
-<div class="footer" style="z-index: 1;">
-
-    <div class="footer-linkbar-container">
-        <div class="footer-icon">
-            <div class="Icon-Frame clickable"><img src="<?=asset_url()?>img/base-rounded.png" srcset="<?=asset_url()?>img/base-rounded@2x.png 2x, <?=asset_url()?>img/base-rounded@3x.png 3x" class="Base-Rounded" onclick="location.href='/';"></div> <div class="OpenBazaar" style="float:left"><a href="/" title="OpenBazaar"><img src="<?=asset_url()?>img/icon-openbazaar-text.png" style="margin-top:22px; width: 100px;" /></a></div>
-        </div>
-
-        <div class="footer-linkbar">
-            <a href="https://openbazaar.org/about">About</a>
-            <a href="https://openbazaar.org/blog">Blog</a>
-            <a href="https://openbazaar.org/developers">Developers</a>
-            <a href="https://openbazaar.org/download">Download</a>
-            <a href="https://openbazaar.org/">Privacy</a>
-        </div>
-    </div>
-
-    <div class="footer-ticker">
-<!--        --><?php //if(isset($_COOKIE['currency']) && $_COOKIE['currency'] != "BTC") { ?>
-            <div class="btc-price ticker-text" style="box-sizing: border-box;padding:8px;padding-left:17px;margin-right:2px;font-size:13px;">
-                <div style="margin-right:10px;font-weight: bold;color: #7A7A7A;">Ticker</div>
-                <img src="<?=asset_url()?>img/btcIcon128.png" /> <?=pretty_price(100000000, "BTC")?>
-                <img src="<?=asset_url()?>img/bchIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(100000000, "BCH")?>
-                <img src="<?=asset_url()?>img/ltcIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(100000000, "LTC")?>
-                <img src="<?=asset_url()?>img/zecIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(100000000, "ZEC")?>
-                <img src="<?=asset_url()?>img/ethIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(1, "ETH")?>
-            </div>
-<!--        --><?php //} ?>
-
-    </div>
-
-</div>
+<?php
+if(!$this->agent->is_mobile()) {
+    $this->load->view('footer_body');
+} else {
+    $this->load->view('footer_body_mobile');
+}
+?>
 
 
 <div id="Config-Modal" style="display: none;"></div>
---
+++ application/views/footer_body_mobile.php
@@ -0,0 +1,5 @@
+<div id="v2-footer-mobile">
+    <a href="/"><img src="<?=base_url()?>/assets/img/home_mobile.png"/></a>
+    <a href="/trending"><img src="<?=base_url()?>/assets/img/trending_mobile.png"/></a>
+    <a href="/new"><img src="<?=base_url()?>/assets/img/new_mobile.png"/></a>
+</div>
