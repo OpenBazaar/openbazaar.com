22a27842774c3c6bb3122aa43a3a06673469077b
Brian Hoffman <brianchoffman@gmail.com>
2020-07-08 11:50:59 -0400

    Fix footer icons

--- application/views/footer_body_mobile.php
+++ application/views/footer_body_mobile.php
@@ -1,5 +1,5 @@
 <div id="v2-footer-mobile">
-    <a href="/"><img src="<?=base_url()?>/assets/img/home_mobile.png"/></a>
-    <a href="/trending"><img src="<?=base_url()?>/assets/img/trending_mobile.png"/></a>
-    <a href="/new"><img src="<?=base_url()?>/assets/img/new_mobile.png"/></a>
+    <a href="/"><img src="<?=base_url()?>/assets/img/home_mobile.png" width="27" height="33"/></a>
+    <a href="/trending"><img src="<?=base_url()?>/assets/img/trending_mobile.png" width="41" height="33"/></a>
+    <a href="/new"><img src="<?=base_url()?>/assets/img/new_mobile.png" width="21" height="31"/></a>
 </div>
