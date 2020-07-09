231f8042ea2d8fbeb1a11d98fb5b71856016279b
Brian Hoffman <brianchoffman@gmail.com>
2020-07-09 13:10:57 -0400

    Trending mobile fix

--- application/views/trending_mobile.php
+++ application/views/trending_mobile.php
@@ -1,5 +1,6 @@
 <div id="v2-bodyContainer-mobile">
-    <input type="hidden" id="v2-page" value="home"/>
+    <input type="hidden" id="v2-page" value="trending"/>
+    <input type="hidden" id="v2-pageNumber" value="0"/>
     <?php $this->load->view('home_filterheader_mobile'); ?>
     <div class="v2-listingContainer"></div>
     <div id="v2-loader"><img src="<?=base_url()?>/assets/img/spinner.gif"/></div>
