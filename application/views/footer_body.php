7325a026f9ec6f103ea86ef7291a697aa43cf51c
Brian Hoffman <brianchoffman@gmail.com>
2020-08-12 20:27:33 -0400

    Fix ticker prices

--- application/views/footer_body.php
+++ application/views/footer_body.php
@@ -15,7 +15,7 @@
     </div>
 
     <div class="footer-ticker">
-        <!--        --><?php //if(isset($_COOKIE['currency']) && $_COOKIE['currency'] != "BTC") { ?>
+        <?php if(isset($_COOKIE['currency']) && $_COOKIE['currency'] != "BTC") { ?>
         <div class="btc-price ticker-text" style="box-sizing: border-box;padding:8px;padding-left:17px;margin-right:2px;font-size:13px;">
             <div style="margin-right:10px;font-weight: bold;color: #7A7A7A;">Ticker</div>
             <img src="<?=asset_url()?>img/btcIcon128.png" /> <?=pretty_price(100000000, "BTC")?>
@@ -24,7 +24,16 @@
             <img src="<?=asset_url()?>img/zecIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(100000000, "ZEC")?>
             <img src="<?=asset_url()?>img/ethIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(1, "ETH")?>
         </div>
-        <!--        --><?php //} ?>
+        <?php } else { ?>
+        <div class="btc-price ticker-text" style="box-sizing: border-box;padding:8px;padding-left:17px;margin-right:2px;font-size:13px;">
+            <div style="margin-right:10px;font-weight: bold;color: #7A7A7A;">Ticker</div>
+            <img src="<?=asset_url()?>img/btcIcon128.png" /> <?=pretty_price(1, "BTC")?>
+            <img src="<?=asset_url()?>img/bchIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(1, "BCH")?>
+            <img src="<?=asset_url()?>img/ltcIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(1, "LTC")?>
+            <img src="<?=asset_url()?>img/zecIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(1, "ZEC")?>
+            <img src="<?=asset_url()?>img/ethIcon128.png" style="margin-left: 15px;" /> <?=pretty_price(1, "ETH")?>
+        </div>
+        <?php } ?>
 
     </div>
 
